1472c1bdbdafc12ae1bf2333e246dc0030e17350
* <shalomimaledo@gmail.com>
2024-04-01 12:59:18 +0100

    FIRST commencement timer

--- shalompedia.py
+++ shalompedia.py
@@ -1,2 +1,26 @@
-print("First commit")
-print("Second commit")
+import time
+from os import system
+classes = {"A": "JSS1", "B": "JSS2", "C": "JSS3", "D": "SS1", "E": "SS2", "F": "SS3"}
+print("   Hello user! What is your name?")
+
+user_name = input("    > ")
+time.sleep(.5)
+print(f"\n\nHello {user_name},\n	> Select your class")
+user_class = input("\n	" + "\n	".join(list(map(lambda one_class: f"{one_class}.  {classes[one_class]}", classes)))  + "\n\n\n	> ")
+print(f"{user_name} is in {classes[user_class.replace('.', '')]}")
+time.sleep(.3)
+system("clear")
+
+
+
+def timer_to_start_quiz(time_length):
+	for t in range(time_length):
+		print(f"Your questions begin in {time_length} second{'s' if time_length > 1 else ''}")
+		time_length -= 1
+		time.sleep(1)
+		system("clear")
+		
+
+
+
+timer_to_start_quiz(5)
